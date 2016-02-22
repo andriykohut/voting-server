52b745f79919e167149b4eabddeeca4e5dd90a9b
Andriy Kogut <kogut.andriy@gmail.com>
2016-02-22 20:48:07 +0200

    add test for immutable list

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
 
@@ -14,6 +15,26 @@ describe('immutability', () => {
       expect(state).to.equal(42);
     });
 
+  });
+
+  describe('a list', () => {
+
+    const addMovie = (currentState, movie) => currentState.push(movie);
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
   });
 
 });
